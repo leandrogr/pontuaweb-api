d9f7317c329d7dc7c7f3d4dc05444785a5dbf7dc
Leandro Rosa <leandrogr03@gmail.com>
2023-03-27 22:00:40 -0300

    Add docker compose file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { CharactersModule } from './characters/characters.module';
 import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [CharactersModule, UsersModule],
